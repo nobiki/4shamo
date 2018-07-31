c41e5c2d646834ca9dfdda5fea968c738960c432
John Smith <john@example.com>
2018-07-31 12:24:55 +0900

    hosts

--- robot.local.sh
+++ robot.local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sed -i -e "s:/path/to/8kiniku:${PWD}:g" ${PWD}/robot.local.conf
+sed -i -e "s:/path/to/4shamo:${PWD}:g" ${PWD}/robot.local.conf
 sudo ln -s ${PWD}/robot.local.conf /etc/nginx/conf.d/robot.local.conf
 sudo systemctl restart nginx
+
+echo "127.0.0.1  robot.local" | sudo tee -a /etc/hosts

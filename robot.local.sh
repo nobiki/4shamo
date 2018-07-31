0b261563a4af767668d15939d4635eedcc5101ce
Naoaki Obiki <obiki.wk@gmail.com>
2018-07-31 16:30:17 +0900

    :+1:

--- robot.local.sh
+++ robot.local.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sed -i -e "s:/path/to/4shamo:${PWD}:g" ${PWD}/robot.local.conf
-sudo ln -s ${PWD}/robot.local.conf /etc/nginx/conf.d/robot.local.conf
+sudo cp ${PWD}/robot.local.conf /etc/nginx/conf.d/robot.local.conf
+git checkout .
 sudo systemctl restart nginx
 
 echo "127.0.0.1  robot.local" | sudo tee -a /etc/hosts

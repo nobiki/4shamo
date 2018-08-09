b56c351f9914aa879f9f6daf013b29f68e3e99a0
Naoaki Obiki <obiki.wk@gmail.com>
2018-08-09 18:17:54 +0900

    :pencil2: add build tag

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+BUILDTAG=`pwgen -A -0 -1 10`
+export BUILDTAG=${BUILDTAG}
+echo "BUILDTAG: ${BUILDTAG}"
+
 robot -A robot.args entry.robot
 
 # robot -A robot.args -t このテスト -t あのテスト client.robot

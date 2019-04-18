5ccf2b3df7125df4b47445651500ef8a4b67a4fd
Naoaki Obiki <obiki.wk@gmail.com>
2019-04-18 15:31:46 +0900

    modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,6 @@ BUILDTAG=`pwgen -A -0 -1 10`
 export BUILDTAG=${BUILDTAG}
 echo "BUILDTAG: ${BUILDTAG}"
 
-robot -A robot.args entry.robot
+robot -A robot.args -v BUILDTAG:${BUILDTAG} entry.robot
 
-# robot -A robot.args -t このテスト -t あのテスト client.robot
+# robot -A robot.args -v BUILDTAG:${BUILDTAG} -t このテスト -t あのテスト client.robot
